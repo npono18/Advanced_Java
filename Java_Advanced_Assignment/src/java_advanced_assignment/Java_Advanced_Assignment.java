4bb1494fe84596d9bfb6d81f46e385c5489591f3
S332629690 <S332629690@LDHS-BCT-131453.ad.ocdsb.ca>
2019-11-27 11:20:41 -0500

    Merge origin/game_screen into game_screen
    Conflicts:
    	2PlayerSnake/src/pkg2playersnake/Game_Board.java

--- Java_Advanced_Assignment/src/java_advanced_assignment/Java_Advanced_Assignment.java
+++ Java_Advanced_Assignment/src/java_advanced_assignment/Java_Advanced_Assignment.java
@@ -10,12 +10,49 @@ package java_advanced_assignment;
  * @author S332629690
  */
 public class Java_Advanced_Assignment {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    enum Directions{
+        N, NE, E, SE, S, SW, W, NW;
+ 
+        Directions direction;
+            this.direction = d;
+        }
     }
     
-}
+    public void guy(Directions x){
+        switch(x){
+            case N:
+                System.out.println("NORTH");
+                break;
+            case NE:
+                System.out.println("NORTHEAST");
+                break;
+            case E:
+                System.out.println("EAST");
+                break;
+            case SE:
+                System.out.println("SOUTHEAST");
+                break;
+            case S:
+                System.out.println("SOUTH");
+                break;
+            case SW:
+                System.out.println("SOUTHWEST");
+                break;
+            case W:
+                System.out.println("WEST");
+                break;
+            case NW:
+                System.out.println("NORTHWEST");
+                break;
+        }
+    }
+    
+    public static void main(String[] args) {
+        String dir = "N";
+        Main garb = new Main(Directions.valueOf(dir));
+        garb.fullDirection();
+    }
+       
+    }
+    
+
