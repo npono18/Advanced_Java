84f85fe0c682451546b48b1d6e05ae4c1c847d95
S332629690 <S332629690@LDHS-BCT-131453.ad.ocdsb.ca>
2019-11-29 11:29:20 -0500

    Merge origin/game_screen into game_screen
    Conflicts:
    	2PlayerSnake/src/pkg2playersnake/Game_Board.java

--- Java_Advanced_Assignment/src/java_advanced_assignment/FileWriterClass.java
+++ Java_Advanced_Assignment/src/java_advanced_assignment/FileWriterClass.java
@@ -1,40 +1,17 @@
 
-package java_advanced_assignment;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-//import java.io.FileWriter;
-//import java.io.BufferedWriter;
-import java.util.List;
 
-
-
-/**
- *
- * @author S332629690
- */
 public class FileWriterClass{
-    public static void main(String[] args) throws Exception{
-        File file = new File("12345.txt");
-        Scanner sc = new Scanner(file);
-        //FileWriter fr = new FileWriter("12345.txt");
-        //BufferedWriter br = new BufferedWriter(fr);
-        //br.write("123456789abcde");
-       // br.close();
-       // fr.close();
-        while (sc.hasNextLine())
-            System.out.println(sc.nextLine());
-        String token1 = "";
-        Scanner inFile1 = new Scanner(new File("12345.txt")).useDelimeter(",\\s*");
-        List<String>data = new ArrayList<String>();
-        while (inFile1.hasnext())
-            token1 = inFile1.next();
-        data.add(token1);
-    }
-    inFile1.close();
-    String[] dataArray = data.toArray(new String[0]);
-    for (String s: dataArray){
-    System.out.println(s);
-}
-            
+    public static void main(String[] args){
+        Scanner sc = new Scanner(new File("12345.txt"));
+        Scanner s = new Scanner(new FileReader("12345.txt"));
         
     }
+}
